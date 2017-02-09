b82a56df66aad66ed42a5a19dc1e8f6550210565
Tom Doel <t.doel@ucl.ac.uk>
2017-02-09 17:56:59 +0000

    Added get_block_coordinate_range()

--- python/pyvesseltools/tests/test_filesplitter.py
+++ python/pyvesseltools/tests/test_filesplitter.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-from .context import tools
+
+from context import tools
 from tools import file_splitter
-from tools.file_splitter import get_number_of_blocks
+from tools.file_splitter import get_number_of_blocks, get_block_coordinate_range
 
 import unittest
 
 
 class TestFileSplitter(unittest.TestCase):
-    """Basic test cases."""
+    """Tests for FileSplitter."""
 
     def test_get_number_of_blocks(self):
         self.assertEqual(get_number_of_blocks([10, 10, 10], [5, 5, 5]), [2, 2, 2])
@@ -15,6 +16,13 @@ class TestFileSplitter(unittest.TestCase):
         self.assertEqual(get_number_of_blocks([11, 11, 11], [5, 5, 5]), [3, 3, 3])
         self.assertEqual(get_number_of_blocks([2, 2, 2], [1, 2, 3]), [2, 1, 1])
         self.assertEqual(get_number_of_blocks([2001, 2000, 1999], [500, 500, 500]), [5, 4, 4])
+
+    def test_get_block_coordinate_range(self):
+        self.assertEqual(get_block_coordinate_range(0, 5, 1, 5), (0, 4))
+        self.assertEqual(get_block_coordinate_range(0, 5, 1, 10), (0, 5))
+        self.assertEqual(get_block_coordinate_range(1, 5, 1, 10), (4, 9))
+        self.assertEqual(get_block_coordinate_range(0, 5, 1, 9), (0, 5))
+        self.assertEqual(get_block_coordinate_range(1, 5, 1, 9), (4, 8))
 
 
 if __name__ == '__main__':
--- python/pyvesseltools/tools/file_splitter.py
+++ python/pyvesseltools/tools/file_splitter.py
@@ -13,3 +13,19 @@ from math import ceil
 def get_number_of_blocks(image_size, max_block_size):
     return [ceil(image_size_element / max_block_size_element) for image_size_element, max_block_size_element in
             zip(image_size, max_block_size)]
+
+
+def get_block_coordinate_range(block_number, block_size, overlap_size, image_size):
+
+    # Compute the minimum coordinate of the block
+    if block_number == 0:
+        min_coord = 0
+    else:
+        min_coord = block_number*block_size - overlap_size
+
+    # Compute the maximum coordinate of the block
+    max_coord = (block_number + 1)*block_size - 1 + overlap_size
+    if max_coord >= image_size:
+        max_coord = image_size - 1
+
+    return min_coord, max_coord
